caf9d42c5c81e97a0559e43ac31a3bf7f10ff219
Chris Malec <cmalec@users.noreply.github.com>
2026-01-28 10:29:56 -0800

    Event schema cleanup (#5)
    * event schema update and cleanup
    
    * fix imports and improve test coverage
    
    ---------
    
    Co-authored-by: masha256 <mike.machado@smartcontract.com>

---
+++ verifiable_event.go
@@ -0,0 +1,46 @@
+package workflows
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"github.com/smartcontractkit/crec-api-go/models"
+)
+
+var ErrNilVerifiableEvent = errors.New("verifiable event cannot be nil")
+
+func EncodeVerifiableEvent(ve *models.VerifiableEvent) (string, error) {
+	if ve == nil {
+		return "", ErrNilVerifiableEvent
+	}
+	jsonString, err := json.Marshal(ve)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(jsonString), nil
+}
+
+func DecodeVerifiableEvent(encoded string) (*models.VerifiableEvent, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return nil, err
+	}
+	var ve models.VerifiableEvent
+	err = json.Unmarshal(decodedBytes, &ve)
+	if err != nil {
+		return nil, err
+	}
+	return &ve, nil
+}
+
+func ComputeEventHash(encodedVerifiableEvent string) (common.Hash, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(encodedVerifiableEvent)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return crypto.Keccak256Hash(decodedBytes), nil
+}
